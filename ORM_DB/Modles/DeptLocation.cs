a8d46525147a5ee88cdc2c312227242315e8a300
Amani <amanialshmali7@gmail.com>
2024-11-05 15:21:12 +0400

    DepLocation class

---
+++ ORM_DB/Modles/DeptLocation.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ORM_DB.Modles
+{
+    [PrimaryKey(nameof(Dnumber), nameof(Dlocation))]
+    public class DeptLocation
+    {
+        
+        [ForeignKey("Department")]
+        public int Dnumber { get; set; }
+        public virtual Department Department { get; set; }
+
+
+        public string Dlocation { get; set; }
+
+    }
+}
