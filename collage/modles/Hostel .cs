81c01f254a876fbd268ded8e2fefaa86b75f56a7
Amani <amanialshmali7@gmail.com>
2024-11-05 16:04:59 +0400

    adding hotal class

---
+++ collage/modles/Hostel .cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace collage.modles
+{
+    public class Hostel
+    {
+        [Key]
+        public int Hostel_id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Hostel_name { get; set; }
+
+        [Required]
+        public int No_of_seats { get; set; }
+
+        [MaxLength(50)]
+        public string City { get; set; }
+
+        [MaxLength(50)]
+        public string State { get; set; }
+
+        [MaxLength(200)]
+        public string Address { get; set; }
+
+        [MaxLength(10)]
+        public string Pin_code { get; set; }
+
+        
+        public ICollection<Student> Students { get; set; }
+    }
+}
