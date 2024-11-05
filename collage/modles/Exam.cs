9d1b6a8ae07d9a10dd0f6f19079a3889a9065d8c
Amani <amanialshmali7@gmail.com>
2024-11-05 16:06:18 +0400

    adding Exam class

---
+++ collage/modles/Exam.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace collage.modles
+{
+    public class Exam
+    {
+
+        [Key]
+        public int Exam_code { get; set; }
+
+        [Required]
+        public DateTime Date { get; set; }
+
+        [Required]
+        [MaxLength(10)]
+        public string Time { get; set; }
+
+        [MaxLength(50)]
+        public string Room { get; set; }
+
+       
+        public ICollection<Student> Students { get; set; }
+    }
+}
