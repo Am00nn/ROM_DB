e164791e6bdb04654e36540ca780368ea20e204e
Amani <amanialshmali7@gmail.com>
2024-11-05 16:01:59 +0400

    add Course class

---
+++ collage/modles/Course .cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace collage.modles
+{
+    public class Course
+    {
+        [Key]
+        public int Course_id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Course_name { get; set; }
+
+        [Required]
+        public int Duration { get; set; }
+
+        public int DepartmentId { get; set; }
+
+       
+        public Department Department { get; set; }
+
+        public ICollection<Student> Students { get; set; }
+    }
+}
