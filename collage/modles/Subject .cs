f86cf6ad0170179cd86e68a5160a56848d3f0b63
Amani <amanialshmali7@gmail.com>
2024-11-05 16:03:35 +0400

    adding Subject class

---
+++ collage/modles/Subject .cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace collage.modles
+{
+    public class Subject
+    {
+        [Key]
+        public int Subject_id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Subject_name { get; set; }
+
+      
+        public ICollection<Faculty> Faculties { get; set; }
+    }
+}
