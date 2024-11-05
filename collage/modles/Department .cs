9e09030a017a53f9c07332db0ed32f5e31026f94
Amani <amanialshmali7@gmail.com>
2024-11-05 16:01:35 +0400

    add Department calss

---
+++ collage/modles/Department .cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace collage.modles
+{
+    public class Department
+    {
+        [Key]
+        public int Department_id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string D_name { get; set; }
+
+       
+        public ICollection<Course> Courses { get; set; }
+        public ICollection<Faculty> Faculties { get; set; }
+    }
+}
